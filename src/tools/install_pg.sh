f4e2403af28954fc24ca6dc938316c2db7da61df
dkalika <dima@covar.com>
2023-05-19 21:42:37 -0400

    remove sudo

--- src/tools/install_pg.sh
+++ src/tools/install_pg.sh
@@ -10,8 +10,8 @@ POSTGIS_VERSION=3.2.5dev
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt install -y zlib1g-dev libreadline-dev libossp-uuid-dev libxml2-dev \
+apt update
+apt install -y zlib1g-dev libreadline-dev libossp-uuid-dev libxml2-dev \
                     libxslt1-dev curl make gcc pkg-config libgeos-dev libxml2-utils \
                     libjson-c-dev proj-bin g++ libsqlite3-dev libtiff-dev \
                     libcurl4-gnutls-dev libprotobuf-c-dev libgdal-dev libsfcgal-dev \
